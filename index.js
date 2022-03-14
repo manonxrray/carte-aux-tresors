b7ef87c9c52f1ba4169f718488af3c0534e747ce
Manon <manon.gerray@gmail.com>
2022-03-14 23:30:53 +0100

    error support & adventurers data storage

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { readFileSync } from 'fs';
 
-// TODO : convert txt file to Json 
 export function textToJson(file) {
   let mapData = new Array();
   // 1) First, we turn our text file into a string
   const string = readFileSync(file, 'utf-8').toString();
   // 2) Then we split each string at the '\n' character, which returns an object of strings 
   mapData = string.split('\n');
+  console.log("🚀 ~ file: index.js ~ line 9 ~ textToJson ~ mapData", mapData)
   // 3) Let's init arrays to sort our data
   let C = [];
   let M = [];
@@ -32,21 +32,26 @@ export function textToJson(file) {
 
   // 5) Looping through our data to fill our previous arrays and format data
   for (let i = 0; i < mapData.length; i++) {
-    if (mapData[i].includes('C', 0)){
+    if (mapData[i][0] === '#'){
+      console.log('ignored comment', mapData[i]);
+    } else if (mapData[i][0] === 'C'){
       C.push(format(mapData[i]));
-    } else if (mapData[i].includes('M', 0)){
+    } else if (mapData[i][0] === 'M'){
       M.push(format(mapData[i]))
-    } else if (mapData[i].includes('T', 0)){
+    } else if (mapData[i][0] === 'T'){
       T.push(format(mapData[i]))
-    } else if (mapData[i].includes('A', 0)){
+    } else if (mapData[i][0] === 'A'){
+      console.log(mapData[i]);
       let formatedArr = mapData[i].slice(4).split('-');
       formatedArr[1] = parseInt(formatedArr[1]);
       formatedArr[2] = parseInt(formatedArr[2]);
       A.push(formatedArr)
+    } else {
+      console.log('Error : character not supported !');
     }
   };
 
-  console.log("🚀 ~ file: index.js ~ line 55 ~ textToJson ~ data", data);
+  console.log("🚀 ~ file: index.js ~ line 49 ~ textToJson ~ data", data);
   return data;
 };
 
@@ -70,7 +75,7 @@ export function createMap(info) {
     map.push(tmpArray)
   };
 
-  console.log("🚀 ~ file: index.js ~ line 65 ~ createMap ~ map", map)
+  console.log("🚀 ~ file: index.js ~ line 73 ~ createMap ~ map", map)
 
   let mountains = info.M;
   let m = 0;
@@ -88,21 +93,37 @@ export function createMap(info) {
     map[treasures[t][1]][treasures[t][0]] = 'T' + '(' + treasures[t][2] + ')'
   };
 
-  let adventurer = info.A;
+  let adventurers = info.A;
   let a = 0;
 
-  for (a; a < adventurer.length; a++){
-    map[adventurer[a][1]][adventurer[a][2]] = 'A' + '(' + adventurer[a][0] + ')'
+  for (a; a < adventurers.length; a++){
+    map[adventurers[a][1]][adventurers[a][2]] = 'A' + '(' + adventurers[a][0] + ')'
   };
 
-  return map;
+  // * Now moving our adventurers and collecting their treasures ! 
+
+  return move(adventurers, map);
 };
 
-// TODO: moving the adventurer function
-// export function move(info) {
-//   const map = createMap(info);
-//   return map
-// }
+export function move(adventurers, map){
+  let a = 0;
+  // Init an object which will contain our adventurers data
+  let data = {};
+
+  // Looping through our adventurers to set up their travel details
+  for (a; a < adventurers.length; a++){
+    let adventurer = adventurers[a][0];
+    data[adventurer] = { "position": [], "orientation": [], "actions": []};
+    data[adventurer].position.push(adventurers[a][1], adventurers[a][2]);
+    data[adventurer].orientation.push(adventurers[a][3]);
+    data[adventurer].actions.push(adventurers[a][4])
+  };
+
+  // TODO conditions and moving adventurer on the map
+  return data;
+};
 
 // Testing what createMap() returns
-console.log("🚀 ~ file: index.js ~ line 111 ~ createMap", createMap(textToJson('./tests/info.txt')));
+console.log("🚀 ~ file: index.js ~ line 53 ~ createMap", createMap(textToJson('./tests/info.txt')));
+// Testing what the move() function returns
+// console.log("🚀 ~ file: index.js ~ line 103 ~ move ~ move", move);
